7d0a0aaaa8987254eee9498277e4950111a56d12
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2021-04-06 22:11:01 -0400

    Calculating remaining time

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,8 @@ const profile = {
     "monthly-budget": 3000,
     "days-per-week": 5,
     "hours-per-day": 5,
-    "vacation-per-year": 4
+    "vacation-per-year": 4,
+    "value-hour": 75
 }
 
 const jobs = [
@@ -29,9 +30,36 @@ const jobs = [
     }
 ]
 
-routes.get('/', (req, res) => res.render(views + "index", {
-    jobs
-})) 
+function remainingDays(job){
+    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+    
+    const createdDate = new Date(job.created_at)
+    const dueDay = createdDate.getDate() + Number(remainingDays) 
+    const dueDateInMs = createdDate.setDate(dueDay)
+    const timeDiffInMs = dueDateInMs - Date.now()
+    const dayInMs = 1000 * 60 * 60 * 24
+    const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+    
+    return dayDiff
+}
+
+routes.get('/', (req, res) => {
+
+    const updatedJobs = jobs.map((job) => {
+        const remaining = remainingDays(job)
+        const status = remaining <= 0 ? 'done' : 'progress'
+
+        return {
+            ...job,
+            remaining,
+            status,
+            budget: profile['value-hour'] * job['total-hours']
+        }
+    })
+
+    return res.render(views + "index", {jobs: updatedJobs})
+}) 
+
 routes.get('/job', (req, res) => res.render(views + "job"))
 // routes.post('/job', (req, res) => {
 
